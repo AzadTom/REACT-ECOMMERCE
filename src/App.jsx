ca37f3b698ba6042e94128dd0e00b0f4de0b8692
azadtom <kumarazad2917@gmail.com>
2023-07-30 12:15:00 +0530

    add necessary dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from 'react';
 
-function App() {
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
   
-  return(
-   <h1>STARTTER-PROJECT</h1>
-  );
-}
+  function App(){
+    const notify = () => toast.success("Yes, it is working!")
 
-export default App
+    return (
+      <div className='flex flex-col justify-center items-center h-screen'>
+        <button onClick={notify} className='px-4 py-2 rounded-[50px] bg-black text-white'>Notify</button>
+        <ToastContainer />
+      </div>
+    );
+  }
+
+  export default App
