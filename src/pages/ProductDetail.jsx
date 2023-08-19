e6ac929de2fdc0b82c4d12db8b089285978e633a
azadtom <kumarazad2917@gmail.com>
2023-08-19 07:54:26 +0530

    fix detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {useProductsData} from '../context/productdata';
 import ProductCard from '../components/ProductCard';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Productlisting from '../components/Productlisting';
+import Counter from '../components/Counter';
 
 
 function ProductDetail() {
@@ -32,37 +36,47 @@ function ProductDetail() {
 
           
          
-      <div className='flex justify-center items-center'>
+    <section key={`${item._id}`}>
 
-      <div className='flex flex-col sm:flex-row justify-around gap-8  p-8 lg:p-16'>
-      <div>
-        <img src={item?.image}  alt="image" className='w-[16rem]' />
-      </div>
-      <div className='flex flex-col gap-4'>
-           
-           <h3 className='font-semibold text-2xl'>{item?.name}</h3>
-           <p>{item?.description}</p>
-           <p>{`$${item?.price}`}</p>
+    <Header/>
+    <div className='flex justify-center items-center'>
 
-           <div>
-            <button className='bg-white text-black px-8 py-2 rounded'>Add To Bag</button>
-           </div>
+     
 
-      </div>
-       
-      </div>
-      </div>
+<div className='flex flex-col sm:flex-row justify-around gap-8  p-8 lg:p-16'>
+<div>
+  <img src={item?.image}  alt="image" className='w-[16rem]' />
+</div>
+<div className='flex flex-col gap-4 justify-start items-start'>
+     
+     <h3 className='font-semibold text-2xl'>{item?.name}</h3>
+     <p>{item?.description}</p>
+     <p>{`$${item?.price}`}</p>
+     <Counter/>
 
-      <h3 className='font-semibold text-2xl px-2 lg:px-16'>Similar Products</h3>
-      <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-8 p-2 sm:p-8 lg:p-16'>
-        {
-          similarProducts.map((item)=>(
-              <ProductCard item={item}/>
+     <div>
+      <button className='bg-white text-black px-8 py-2 rounded'>Add To Bag</button>
+     </div>
 
-          ))
-          
-        }
-      </div>
+</div>
+ 
+</div>
+</div>
+
+<h3 className='font-semibold text-2xl px-2 lg:px-16'>Similar Products</h3>
+
+  {
+   
+
+    <Productlisting/>
+    
+  }
+
+
+
+    <Footer/>
+
+    </section>
        
     
     </>
