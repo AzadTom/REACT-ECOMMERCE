44b1f782b8336cc80c94d646b118f364ace0e662
Azadkumar <105869036+AzadTom@users.noreply.github.com>
2023-12-28 22:26:54 +0530

    Update GetProducts.jsx

--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useProductsData } from "../context/productdata";
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';  
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 import { useNavigate } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
@@ -10,12 +10,14 @@ import ProductCard from "./ProductCard";
 function GetProducts() {
   const { productsData } = useProductsData();
 
+  const[curr,setCurr]= useState(0);
+
 
    const navigate = useNavigate();
   const leftscroll = ()=>{
 
 
-    document.getElementById("content").style.transform = 'translateX(-400px)';
+      setCurr((prev)=>(prev>5?0:prev-1);
 
 
   }
@@ -23,7 +25,7 @@ function GetProducts() {
   const rightscroll = ()=>{
 
 
-        document.getElementById("content").style.transform = 'translateX(400px)';
+       setCurr((prev)=>(prev>5?0:prev+1);
 
   }
 
@@ -34,11 +36,11 @@ function GetProducts() {
          <h1 className="font-semibold text-center text-xl">All Products</h1>
            
     
-      <div id="content" className="flex justify-center relative">
+      <div  className="flex justify-center relative">
         
         <div className="overflow-clip">
         <div className="flex flex-no-wrap overflow-x-auto scroll-smooth scrollbar-hide  gap-6  m-8   ">
-        {productsData.map((item) => ( <div className="w-[250px]"> <ProductCard item={item}/></div>))}
+        {productsData.map((item) => ( <div className="w-[250px]"> <ProductCard item={item} curr={curr}/></div>))}
       </div>
       <button onClick={leftscroll} className="bg-white p-2 m-2 text-black rounded absolute top-1/3 left-0"> <ArrowCircleLeftIcon/></button>
      <button onClick={rightscroll} className="bg-white p-2 m-2 text-black rounded absolute top-1/3 right-0"><ArrowCircleRightIcon/></button>
